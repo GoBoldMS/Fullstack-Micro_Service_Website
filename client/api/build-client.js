d3afcb43a5f64d0fd0ac718c59166094f4d9a9eb
yonatan <levinjonatan80@gmail.com>
2023-04-09 00:31:03 +0300

    build client bug fix

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const Api = ({ req }) => {
     if (typeof window === 'undefined') {
         return axios.create({
-            baseURL: "http://www.ticketing-app-mok-prod.xyz/api",
+            baseURL: "http://www.ticketing-app-mok-prod.xyz",
             headers: req.headers
         });
     } else {
